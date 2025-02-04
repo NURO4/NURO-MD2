4d576e3a4f919db3a72becabd5447ae491103811
NURO4 <tharakadilshan581@gmail.com>
2025-02-04 12:11:41 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,41 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require("os")
+cmd({
+    pattern: "system",
+    alias: ["status","info"],
+    react: "🤖",
+    desc: "Check up time , ram usage and more",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let status = `
+*[ •  NURO-MD - UPTIME ‎ • ]*
+*╭┈───────────────•*
+*│  ◦* *_UPTIME:➠_*
+*│  ◦* ${runtime(process.uptime())}
+*│  ◦*
+*│  ◦* *_RAM USAGE:➠_*
+*│  ◦* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*│  ◦*
+*│  ◦* *_HOSTNAME:➠_*
+*│  ◦* ${os.hostname()}
+*│  ◦*
+*│  ◦* *_PLATFORM:➠_*
+*│  ◦* ${process.env.DYNO ? "Heroku" : "Localhost"}
+*│  ◦*
+*╰┈───────────────•*
+
+*•────────────•⟢*
+> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴜʀᴏ
+*•────────────•⟢*
+`
+return reply(`${status}`)
+}catch(e){
+console.log(e)
+reply(`${e}`)  
+
+}
+})
